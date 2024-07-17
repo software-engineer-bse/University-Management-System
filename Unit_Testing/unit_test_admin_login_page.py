cb44a2d494da3bcd8beb78b2b2c872a45abf973f
software-engineer-bse <khurram.email.123@gmail.com>
2024-07-17 06:29:06 -0700

    add Unit_Testing/unit_test_admin_login_page.py

---
+++ Unit_Testing/unit_test_admin_login_page.py
@@ -0,0 +1,32 @@
+import time
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class unit_test_admin_login_page(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Firefox()
+        cls.driver.get("http://127.0.0.1:5000/admin")
+        cls.driver.maximize_window()
+        
+    def test_admin_login_positive(self):
+        driver = self.driver
+        driver.find_element(By.ID, "username").send_keys("admin_1")
+        driver.find_element(By.ID, "password").send_keys("admin_1")
+        driver.find_element(By.XPATH, "/html/body/div/div/div/div/form/input").click()
+        time.sleep(2)
+    
+    
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
+        
+        
+        
+if __name__=="__main__":
+    unittest.main()
